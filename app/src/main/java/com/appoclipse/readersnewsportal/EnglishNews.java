65170cdee4c474ec71c40dbda8ae99e52cfea03b
Shahriyar Parvez Shuvo <mr.shahriyarshuvo@gmail.com>
2017-08-05 23:27:56 +0600

    English news Done

--- app/src/main/java/com/appoclipse/readersnewsportal/EnglishNews.java
+++ app/src/main/java/com/appoclipse/readersnewsportal/EnglishNews.java
@@ -20,7 +20,7 @@ public class EnglishNews extends AppCompatActivity {
 
     public void dailyStar(View view){
 
-        String title = "The Daily Star";
+        String title = "Daily Star";
         Intent intent = new Intent(this, WebViewClass.class);
         intent.putExtra("urlString", "http://www.thedailystar.net");
         intent.putExtra("titleText", title);
@@ -33,7 +33,7 @@ public class EnglishNews extends AppCompatActivity {
     public void dailySun(View view){
 
 
-        String title = "The Daily Sun";
+        String title = "Daily Sun";
         Intent intent = new Intent(this, WebViewClass.class);
         intent.putExtra("urlString", "http://www.daily-sun.com");
         intent.putExtra("titleText", title);
@@ -44,7 +44,7 @@ public class EnglishNews extends AppCompatActivity {
 
     public void bdToday(View view){
 
-        String title = "The Bangladesh Today";
+        String title = "BD Today";
         Intent intent = new Intent(this, WebViewClass.class);
         intent.putExtra("urlString", "http://thebangladeshtoday.com");
         intent.putExtra("titleText", title);
@@ -54,7 +54,7 @@ public class EnglishNews extends AppCompatActivity {
     }
     public void newAge (View view){
 
-        String title = "The New Age";
+        String title = "New Age";
         Intent intent = new Intent(this, WebViewClass.class);
         intent.putExtra("urlString", "http://newagebd.net");
         intent.putExtra("titleText", title);
@@ -65,7 +65,7 @@ public class EnglishNews extends AppCompatActivity {
 
     public void newsToday (View view){
 
-        String title = "The News Today";
+        String title = "News Today";
         Intent intent = new Intent(this, WebViewClass.class);
         intent.putExtra("urlString", "http://www.newstoday.com.bd");
         intent.putExtra("titleText", title);
@@ -76,7 +76,7 @@ public class EnglishNews extends AppCompatActivity {
 
     public void independent (View view){
 
-        String title = "The Independent";
+        String title = "Independent";
         Intent intent = new Intent(this, WebViewClass.class);
         intent.putExtra("urlString", "http://www.theindependentbd.com");
         intent.putExtra("titleText", title);
@@ -87,7 +87,7 @@ public class EnglishNews extends AppCompatActivity {
 
     public void newNation (View view){
 
-        String title = "The New Nation";
+        String title = "New Nation";
         Intent intent = new Intent(this, WebViewClass.class);
         intent.putExtra("urlString", "http://www.thedailynewnation.com");
         intent.putExtra("Integer", 2);
@@ -123,7 +123,7 @@ public class EnglishNews extends AppCompatActivity {
 
     public void asianAge (View view){
 
-        String title = "Daily Asian Age";
+        String title = "Asian Age";
         Intent intent = new Intent(this, WebViewClass.class);
         intent.putExtra("urlString", "http://www.dailyasianage.com/");
         intent.putExtra("titleText", title);
@@ -145,6 +145,109 @@ public class EnglishNews extends AppCompatActivity {
 
 
 
+    public void nytimes (View view){
+
+        String title = "NY Times";
+        Intent intent = new Intent(this, WebViewClass.class);
+        intent.putExtra("urlString", "https://nytimes.com/");
+        intent.putExtra("titleText", title);
+        intent.putExtra("Integer", 2);
+        startActivity(intent);
+        finish();
+    }
+
+    public void guardian (View view){
+
+        String title = "The Guardian";
+        Intent intent = new Intent(this, WebViewClass.class);
+        intent.putExtra("urlString", "https://theguardian.com");
+        intent.putExtra("titleText", title);
+        intent.putExtra("Integer", 2);
+        startActivity(intent);
+        finish();
+    }
+
+    public void huffington (View view){
+
+        String title = "Huffington Post";
+        Intent intent = new Intent(this, WebViewClass.class);
+        intent.putExtra("urlString", "http://huffingtonpost.com/");
+        intent.putExtra("titleText", title);
+        intent.putExtra("Integer", 2);
+        startActivity(intent);
+        finish();
+    }
+
+    public void washington (View view){
+
+        String title = "Washington Post";
+        Intent intent = new Intent(this, WebViewClass.class);
+        intent.putExtra("urlString", "https://washingtonpost.com/");
+        intent.putExtra("titleText", title);
+        intent.putExtra("Integer", 2);
+        startActivity(intent);
+        finish();
+    }
+
+
+    public void usatoday (View view){
+
+        String title = "USA Today";
+        Intent intent = new Intent(this, WebViewClass.class);
+        intent.putExtra("urlString", "http://usatoday.com/");
+        intent.putExtra("titleText", title);
+        intent.putExtra("Integer", 2);
+        startActivity(intent);
+        finish();
+    }
+
+
+    public void bloomberg (View view){
+
+        String title = "Bloomberg";
+        Intent intent = new Intent(this, WebViewClass.class);
+        intent.putExtra("urlString", "https://bloomberg.com/");
+        intent.putExtra("titleText", title);
+        intent.putExtra("Integer", 2);
+        startActivity(intent);
+        finish();
+    }
+
+
+    public void nypost(View view){
+
+        String title = "NY Post";
+        Intent intent = new Intent(this, WebViewClass.class);
+        intent.putExtra("urlString", "http://nypost.com/");
+        intent.putExtra("titleText", title);
+        intent.putExtra("Integer", 2);
+        startActivity(intent);
+        finish();
+    }
+
+    public void hindutimes (View view){
+
+        String title = "HT";
+        Intent intent = new Intent(this, WebViewClass.class);
+        intent.putExtra("urlString", "http://hindustantimes.com/");
+        intent.putExtra("titleText", title);
+        intent.putExtra("Integer", 2);
+        startActivity(intent);
+        finish();
+    }
+
+    public void indianexpress(View view){
+
+        String title = "Indian Express";
+        Intent intent = new Intent(this, WebViewClass.class);
+        intent.putExtra("urlString", "http://indianexpress.com/");
+        intent.putExtra("titleText", title);
+        intent.putExtra("Integer", 2);
+        startActivity(intent);
+        finish();
+    }
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -153,11 +256,17 @@ public class EnglishNews extends AppCompatActivity {
         final NativeExpressAdView adView1 = (NativeExpressAdView)findViewById(R.id.ads1);
         final NativeExpressAdView adView2 = (NativeExpressAdView)findViewById(R.id.ads2);
         final NativeExpressAdView adView3 = (NativeExpressAdView)findViewById(R.id.ads3);
+        final NativeExpressAdView adView4 = (NativeExpressAdView)findViewById(R.id.ads4);
+        final NativeExpressAdView adView5 = (NativeExpressAdView)findViewById(R.id.ads5);
+        final NativeExpressAdView adView6 = (NativeExpressAdView)findViewById(R.id.ads6);
 
         AdRequest request = new AdRequest.Builder().build();
         adView1.loadAd(request);
         adView2.loadAd(request);
         adView3.loadAd(request);
+        adView4.loadAd(request);
+        adView5.loadAd(request);
+        adView6.loadAd(request);
 
 
         adView1.setAdListener(new AdListener() {
@@ -173,6 +282,21 @@ public class EnglishNews extends AppCompatActivity {
         adView3.setAdListener(new AdListener() {
             @Override public void onAdFailedToLoad(int error) {adView3.setVisibility(View.GONE);}
             @Override public void onAdLoaded() {adView3.setVisibility(View.VISIBLE);}
+        });
+
+        adView4.setAdListener(new AdListener() {
+            @Override public void onAdFailedToLoad(int error) {adView4.setVisibility(View.GONE);}
+            @Override public void onAdLoaded() {adView4.setVisibility(View.VISIBLE);}
+        });
+
+        adView5.setAdListener(new AdListener() {
+            @Override public void onAdFailedToLoad(int error) {adView5.setVisibility(View.GONE);}
+            @Override public void onAdLoaded() {adView5.setVisibility(View.VISIBLE);}
+        });
+
+        adView6.setAdListener(new AdListener() {
+            @Override public void onAdFailedToLoad(int error) {adView6.setVisibility(View.INVISIBLE);}
+            @Override public void onAdLoaded() {adView6.setVisibility(View.VISIBLE);}
         });
 
     }
